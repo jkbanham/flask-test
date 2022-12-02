359fe41832d5d1977a4e723a287471855f79c29c
jkbanham <54941260+jkbanham@users.noreply.github.com>
2022-12-02 13:52:47 -0500

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -1,9 +1,40 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 12:42:36 2022
+@author: Hagar
+"""
 
-app = Flask(__name__)
+from flask import Flask, render_template, request, render
+app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Web App with Python Flask!'
-
-app.run(host='0.0.0.0', port=8080)
+""" this Function render the home page"""
+@app.route("/", methods=["GET","POST"])
+def home():
+    if request.method =="POST":
+            name=request.form.get("name")
+            size= float(request.form.get("size"))
+            crema=request.form.get("crema")
+            quantity= int(request.form.get("quantity"))
+            #check the cake size
+            if size == "8":
+                sizeValue=8
+            elif size == "10":
+                sizeValue=10
+            else:
+                sizeValue= 16
+                        
+            #Calculate Total 
+            price= (sizeValue * quantity)
+            total=price+(price*0.06)
+            #return render("Yourorder.html").format(name=name,size=size,quantity=quantity,total=total,crema=crema)
+            print("<html><body>Your order is here</body></html>")
+        
+    else:    
+        
+            #return render("home.html")
+            print("<html><body>Home page is here</body></html>")
+                           
+                           
+if __name__ =="__main__" :
+    
+        app.run(host='0.0.0.0', port=8080)
